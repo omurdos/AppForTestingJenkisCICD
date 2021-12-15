7d5350a709aad77017d532bc511808aaa9cf42e9
Omar Murdos <46492436+omurdos@users.noreply.github.com>
2021-12-15 10:06:08 +0400

    update weather controller

--- AppForTestingJenkisCICD/Controllers/WeatherForecastController.cs
+++ AppForTestingJenkisCICD/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace AppForTestingJenkisCICD.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            pssssst
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
